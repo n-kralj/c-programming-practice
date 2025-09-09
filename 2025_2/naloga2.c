82270c2945c82c1f69ebad7903f6e058d388216f
NKralj <160857318+n-kralj@users.noreply.github.com>
2025-09-10 00:25:00 +0200

    Update naloga2.c

--- 2025_2/naloga2.c
+++ 2025_2/naloga2.c
@@ -1,20 +1,4 @@
 
-/*
- * Zagon posami"cnega primera:
- *
- * gcc naloga2.c
- * ./a.out $(cat test01.param)
- *
- * Zagon testne skripte ("sele potem, ko ste prepri"cani, da program deluje!):
- *
- * export name=naloga2
- * make test
- *
- * Javni testni primeri:
- * 02--04: k = 1
- * 01, 05--10: splo"sni
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
